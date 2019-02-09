9caaa0f506a78aafcc34a4ca373168b15baf3465
Kimberly Losenara <klosenara@gmail.com>
2019-02-09 14:50:24 -0800

    WIP drop down menu

---
+++ scripts/mobilemenu.js
@@ -0,0 +1,13 @@
+function openMenu() {
+    document.querySelector(".mobileMenu").classList.add("open");
+    document.querySelector(".menuList").classList.add("appear");
+}
+
+function closeMenu() {
+    console.log("Hello");
+    document.querySelector(".mobileMenu").classList.remove("open");
+}
+
+function removeClass() {
+    document.querySelector(".mobileMenu").classList.remove("test");
+}
